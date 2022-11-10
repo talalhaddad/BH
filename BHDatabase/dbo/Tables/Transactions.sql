d077ddf01516308aeea5ec790a25959ff324333f
Talal Haddad <tahaddad@microsoft.com>
2022-11-10 12:54:26 +0300

    add to GitHub

--- BHDatabase/dbo/Tables/Transactions.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Transactions] (
-    [TransactionID]       NVARCHAR (450) CONSTRAINT [DF_Transactions_TransactionID] DEFAULT (newid()) NOT NULL,
-    [UserID]              NVARCHAR (450) NOT NULL,
-    [TransactionAmount]   MONEY          NOT NULL,
-    [TransactionDateTime] DATETIME       NOT NULL,
-    CONSTRAINT [PK_Transactions] PRIMARY KEY CLUSTERED ([TransactionID] ASC)
-);
-
